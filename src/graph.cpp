e9905c0913ec5fa4feca4f2b98bab138aad74938
Tim Smith <timothy.smith@cdtm.de>
2023-11-01 16:27:17 +0100

    v0.1 working linear sequencer with GUI and animation

--- src/graph.cpp
+++ src/graph.cpp
@@ -26,21 +26,22 @@ void Graph::init(int numNodes) {
 }
 
 void Graph::activateNext() {
-    int activeIndex;
+    int activeIndex = -1;
     for (int i = 0; i < bubbles.size(); i += 1) {
         if (bubbles[i].active == true) {
             activeIndex = i;
             break;
         }
     }
-    if (!activeIndex) {
+    if (activeIndex >= 0)
+    {
+        bubbles[activeIndex].deactivate();
+    }
+ 
+    if (activeIndex == bubbles.size() - 1) {
         bubbles[0].activate();
     } else {
-        bubbles[activeIndex].deactivate();
-        if (activeIndex == bubbles.size() - 1) {
-            bubbles[0].activate();
-        } else {
-            bubbles[activeIndex+1].activate();
-        }
+        bubbles[activeIndex+1].activate();
     }
+
 }
