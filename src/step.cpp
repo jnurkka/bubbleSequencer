9d5e9a8ed33506e5a95599ad9066eb37571128d9
Jaakko Nurkka <jaakko@cliniserve.de>
2023-11-05 13:06:13 +0100

    feat: set probabilities for each sound

--- src/step.cpp
+++ src/step.cpp
@@ -7,19 +7,26 @@
 
 #include "step.hpp"
 #include "bubble.hpp"
+#include <tuple>
 
-string const FILENAME[5] = {"f.wav", "g.wav", "a.wav", "c.wav", "d.wav" };
 
 Step::Step(int size) : bubbles(size) {
     
 }
 
 void Step::init(int size, int xPos, int bubbleRadius) {
+    using FilenameProbabilityPair = std::tuple<std::string, float>;
+    std::vector<FilenameProbabilityPair> filenameProbabilities;
+    filenameProbabilities.push_back(std::make_tuple("f.wav", 0.2f));
+    filenameProbabilities.push_back(std::make_tuple("g.wav", 0.2f));
+    filenameProbabilities.push_back(std::make_tuple("a.wav", 0.2f));
+    filenameProbabilities.push_back(std::make_tuple("c.wav", 0.2f));
+    filenameProbabilities.push_back(std::make_tuple("d.wav", 0.2f));
+    
     int distNodes = (ofGetWindowHeight() - 2*bubbleRadius) / size;
     int y = bubbleRadius + distNodes / 2;
     for (int i = 0; i < size; i += 1) {
-        string filename = FILENAME[int(ofRandom(FILENAME->size()))];
-        bubbles[i].init(xPos, y, bubbleRadius, filename, 1);
+        bubbles[i].init(xPos, y, bubbleRadius, get<0>(filenameProbabilities[i]), get<1>(filenameProbabilities[i]));
         y += distNodes;
     }
 }
